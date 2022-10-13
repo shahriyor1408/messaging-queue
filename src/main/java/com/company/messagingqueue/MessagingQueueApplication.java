359858b08286681d594206da2a06e24e99930696
Shalhriyor1408 <sohidjonovshahriyor1408@gmail.com>
2022-10-13 10:50:54 +0500

    rabbit-mq-commit

---
+++ src/main/java/com/company/messagingqueue/MessagingQueueApplication.java
@@ -0,0 +1,13 @@
+package com.company.messagingqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingQueueApplication.class, args);
+    }
+
+}
---
+++ subscriber/src/main/java/com/company/subscriber/SubscriberApplication.java
@@ -0,0 +1,94 @@
+package com.company.subscriber;
+
+import lombok.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@SpringBootApplication
+@EnableRabbit
+public class SubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberApplication.class, args);
+	}
+
+}
+
+@Configuration
+class RabbitMQConfig {
+	public static final String QUEUE = "pdp_queue";
+	public static final String EXCHANGE = "pdp_exchange";
+	public static final String ROUTING_KEY = "pdp_routing_key";
+
+	@Bean
+	public Queue queue() {
+		return new Queue(QUEUE);
+	}
+
+	@Bean
+	public TopicExchange exchange() {
+		return new TopicExchange(EXCHANGE);
+	}
+
+
+	@Bean
+	public Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder
+				.bind(queue)
+				.to(exchange)
+				.with(ROUTING_KEY);
+	}
+
+	@Bean
+	public MessageConverter messageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(messageConverter());
+		return rabbitTemplate;
+	}
+
+}
+
+@Component
+class TransactionListener {
+
+	@RabbitListener(queues = RabbitMQConfig.QUEUE)
+	public void logTransaction(Transaction transaction) {
+		System.out.println(transaction);
+	}
+}
+
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+class Transaction {
+	private Long id;
+	private String pan;
+	private BigDecimal amount;
+	private Timestamp createdAt;
+
+}
+
